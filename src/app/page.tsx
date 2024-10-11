7589a915db04b7b1c488143019c2e2f12f52a4c4
Tahirah Roohi <tahirahroohi0@gmail.com>
2024-10-10 18:46:45 -0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,66 @@
-import Image from "next/image";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faUser, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false;
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="bg-green-900 min-h-screen px-9 pt-2 pb-10">
+      <nav className="flex justify-between items-center text-white mb-10">
+        <div className="text-lg font-bold">
+          <img src="/logo.png" alt="Plantify Logo" className="w-32 h-auto"/>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <ul className="flex space-x-8">
+          <li>Shop</li>
+          <li>Products</li>
+          <li>Fertilizer</li>
+          <li>Guide</li>
+        </ul>
+        <div className="flex space-x-4">
+          <FontAwesomeIcon icon={faSearch} />
+          <FontAwesomeIcon icon={faUser} />
+          <FontAwesomeIcon icon={faShoppingCart} />
+        </div>
+      </nav>
+
+      <div className="flex justify-between items-start text-white space-x-20">
+        <div className="max-w-md">
+          <h1 className="text-4xl mt-28 font-bold">Happiness blooms from within</h1>
+          <p className="mt-4">
+            Our environment, the world in which we live and work, is a mirror of our attitudes and expectations.
+          </p>
+          <div className="mt-6 flex space-x-4">
+            <button className="bg-white text-green-900 px-6 py-2 rounded">Shop now</button>
+            <button className=" px-6 py-2">Explore plants</button>
+          </div>
+          </div>
+
+        <div className="grid grid-cols-2 gap-4 max-w-max">
+          <div className="bg-white overflow-hidden transition-transform transform hover:scale-105">
+            <div className="text-green-900 p-4">Now</div>
+            <img src="/image 1.png" alt="Plant" className="w-full h-64  rounded-t-md"/>
+          </div>
+          <div className="bg-white overflow-hidden shadow-lg transition-transform transform hover:scale-105">
+            <div className="text-green-900 p-4">Popular</div>
+            <img src="/image 2.png" alt="Plant" className="w-full h-64 rounded-t-md"/>
+          </div>
+
+          <div className="bg-white overflow-hidden shadow-lg transition-transform transform hover:scale-105">
+            <div className="text-green-900 p-4">Featured</div>
+            <img src="/image 3.png" alt="Anthurium Flower" className="w-full h-64 rounded-t-md"/>
+          </div>
+
+          <div className="bg-white overflow-hidden shadow-lg transition-transform transform hover:scale-105">
+            <div className="text-green-900 p-4">Latest</div>
+            <img src="/image 3.png" alt="Anthurium Flower" className="w-full h-64 rounded-t-md"/>
+          </div>
+
+        </div>
+      </div>
     </div>
+ 
   );
-}
+};
+
+export default Home;
